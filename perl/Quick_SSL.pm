765e4f296d83ac9f3e3c38ab8d054a4f7e3d566f
MicheleMorelli <michele.morelli@london.ac.uk>
2018-06-30 10:42:41 +0000

    better pod

--- perl/Quick_SSL.pm
+++ perl/Quick_SSL.pm
@@ -6,29 +6,12 @@ use Exporter;
 use Crypt::OpenSSL::RSA;
 use Crypt::OpenSSL::Random;
 
-=pod
-
-=head1 Quick SSL
-
-This functions are aimed at dealing open_SSL-related tasks in a quick 
-and automated way.
-
-=cut
 
 our @ISA = qw/ Exporter /;
 our @EXPORT = qw/ $funct /;
 
 our $funct = {};
 
-
-=pod
-
-=head2 ll
-
-Similar to bash's ls -l, it prints the contents of the working directory,
-including the read/write privileges.
-
-=cut
 
 $funct->{ll} = sub {
     opendir my($dh), '.' or die "cannot open dir: $!"; 
@@ -40,14 +23,6 @@ $funct->{ll} = sub {
 };
 
 
-=pod
-
-=head2 make_private_key
-
-Generates a private key and stores it in a .key file.
-
-=cut
-
 $funct->{make_private_key} = sub{
     my ( $SITECODE) = @_;
     # generating the private key
@@ -58,13 +33,6 @@ $funct->{make_private_key} = sub{
     close $fh;
 };
 
-=pod
-
-=head2 make_read_only
-
-Makes a file read only (chmod 400)
-
-=cut
 
 $funct->{make_read_only} = sub{
     my ( $SITECODE) = @_;
@@ -75,3 +43,25 @@ $funct->{make_read_only} = sub{
 };
 
 1;
+
+
+__END__
+
+
+=pod
+
+=head1 Quick SSL
+
+This functions are aimed at dealing open_SSL-related tasks in a quick 
+and automated way.
+
+=head2 ll
+
+Similar to bash's ls -l, it prints the contents of the working directory,
+including the read/write privileges.
+
+=head2 make_private_key
+
+Generates a private key and stores it in a .key file.
+
+=cut
