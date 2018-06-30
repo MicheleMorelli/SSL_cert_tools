2e4d057712c2df7bbb2bfe2c1812cc98c1a67791
MicheleMorelli <michele.morelli@london.ac.uk>
2018-06-30 01:25:31 +0000

    decluttering dir

--- check_moduli.sh
+++
@@ -1,7 +0,0 @@
-# bin/bash
-
-read -p "SITECODE:" SITECODE
-
-openssl x509 -in $SITECODE.crt -noout -modulus | md5sum
-openssl rsa -in $SITECODE.key -noout -modulus | md5sum
-openssl req -in $SITECODE.csr -noout -modulus | md5sum
--- get_crt_info.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-read -p "SITECODE: " SITECODE
-SUBJ=$(openssl x509 -in $SITECODE.crt -noout -text | grep  "^\s*Subject:")
-
-
