ba57995fa50b22f37246512f524e50ced2cc60db
MicheleMorelli <michele.morelli@london.ac.uk>
2018-07-01 19:13:51 +0000

    putting some checks in the check_moduli script

--- check_moduli.sh
+++ check_moduli.sh
@@ -1,7 +1,12 @@
 # bin/bash
 
-read -p "SITECODE:" SITECODE
 
-openssl x509 -in $SITECODE.crt -noout -modulus | md5sum
-openssl rsa -in $SITECODE.key -noout -modulus | md5sum
-openssl req -in $SITECODE.csr -noout -modulus | md5sum
+[[ $# -ne 1 ]] && echo 'ERROR: A sitecode must be passed as a value.
+Terminating..' && exit 1
+SITECODE=$1
+
+[[ -e $SITECODE.crt ]] && CRT=$(openssl x509 -in $SITECODE.crt -noout -modulus | md5sum) 
+[[ -e $SITECODE.key ]] && KEY=$(openssl rsa -in $SITECODE.key -noout -modulus | md5sum)
+[[ -e $SITECODE.csr ]] && CSR=$(openssl req -in $SITECODE.csr -noout -modulus | md5sum)
+
+[[ $CRT == $CRT ]] && echo "OK!"
