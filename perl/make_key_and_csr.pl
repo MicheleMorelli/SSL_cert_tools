15549f2b9ad97e11706927ab399274b0f8719e1a
MicheleMorelli <michele.morelli@london.ac.uk>
2018-07-01 13:16:48 +0000

    refactored demo pl

--- perl/make_key_and_csr.pl
+++ perl/make_key_and_csr.pl
@@ -1,13 +1,13 @@
 #! /usr/bin/perl -w
 
 use strict;
-use Quick_SSL;
-use GetOpt::Long qw(GetOptions);
-Getopt::Long::Configure qw(gnu_getopt);
+use Quick_SSL qw/ $f /;
+use GetOpt::Long qw/ GetOptions /;
+Getopt::Long::Configure qw/ gnu_getopt /;
 
 my $SITECODE = $ARGV[0];
 
-$f->{make_private_key}($SITECODE);
-$f->{make_read_only}($SITECODE);
+$f->{make_private_key}("$SITECODE.key");
+$f->{make_read_only}("$SITECODE.key");
 #TODO: make CSR!
 $f->{ll}();
