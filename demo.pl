6fc4f7b60eeca5729d266016cc6418e82d329d2a
MicheleMorelli <michele.morelli@london.ac.uk>
2020-01-29 12:29:37 +0000

    improved demo doc

--- demo.pl
+++ demo.pl
@@ -1,5 +1,11 @@
 #! /usr/bin/perl -w
 
+=begin comment
+Just some potential example uses of the Quick_SSL package... 
+=end comment
+=cut
+
+
 use strict;
 use Quick_SSL;
 use Data::Dumper;
@@ -9,7 +15,7 @@ my $c = Quick_SSL->new();
 
 #$c->print_sorted_expiry_dates("list.list")
 #my $cert = $c->get_cert_from_site("research.gold.ac.uk");
-$c->make_email("TEST.csr");
+#$c->make_email("TEST.csr");
 
 #print Dumper $c->get_subject("TEST.crt");
 #print $cert->as_string();
